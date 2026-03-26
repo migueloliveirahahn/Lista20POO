d39fd9fe3d63b523b798b7625c8a22b8662289d2
migueloliveirahahn <hahnmiguel26@gmail.com>
2026-03-26 15:03:28 -0300

    Entrega atividade 10

---
+++ Nova pasta/Atividade10/src/atividade10/Atividade10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade10;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Atividade10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
